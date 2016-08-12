55605829b79329d807a6ed0def37e87f5cdc8a9d
mharyst <mykhailo.haryst@gmail.com>
2016-08-12 20:32:20 +0300

    Added badge, sliders, video sections.

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@
 document.querySelector('.dropbtn').addEventListener('click', function () {
   document.querySelector('.dropdown').classList.toggle('active');
 });
+
+function sliderChange(value) {
+  rangevalue.value = value;
+  rangevalue.style = 'left:' + value + '%;';
+}
+function sliderChange2(value) {
+  rangevalue2.value = value;
+  rangevalue2.style = 'left:' + value + '%;';
+}
